2f75a313b696fd0deae880f48e40f892c65f37b4
ProjectRomulo <projectromulo@gmail.com>
2025-01-11 13:14:42 +0100

    Update test_remo.py

--- test_remo.py
+++ test_remo.py
@@ -3,25 +3,7 @@ import numpy as np
 from romulo_colab.remo_core import similitud_coseno, extraer_palabras_clave, analizar_texto
 
 class TestRemo(unittest.TestCase):
-    #Pruebas de similitud coseno
-    def test_similitud_coseno_con_vectores_cero(self):
-        self.assertEqual(similitud_coseno(np.array([0,0]), np.array([0,0])), 0.0)
-
-    def test_similitud_coseno_con_vectores_iguales(self):
-        self.assertEqual(similitud_coseno(np.array([1,1]),np.array([1,1])), 1.0)
-
-    def test_similitud_coseno_con_vectores_opuestos(self):
-        self.assertEqual(similitud_coseno(np.array([1,1]),np.array([-1,-1])), -1.0)
-
-    def test_similitud_coseno_con_vectores_diferentes(self):
-        vector1 = np.array([1, 2, 3])
-        vector2 = np.array([4, 5, 6])
-        resultado_esperado = 0.992277872233
-        self.assertAlmostEqual(similitud_coseno(vector1, vector2), resultado_esperado, places=7)
-
-    def test_extraer_palabras_clave_texto_vacio(self):
-      self.assertEqual(extraer_palabras_clave(""),[])
-
+    # ... (resto de las pruebas)
     def test_extraer_palabras_clave_con_texto(self):
       texto = "Este es un texto de prueba"
       palabras_clave = extraer_palabras_clave(texto)
